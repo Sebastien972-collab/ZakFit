99b44b1f14bd399fba04b1f937efe74e1cda813a
Sébastien DAGUIN <sebastien.daguin94@gmail.com>
2025-11-17 12:48:28 +0100

    Initial Commit

---
+++ ZakFit/ZakFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZakFitApp.swift
+//  ZakFit
+//
+//  Created by Sébastien DAGUIN on 17/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ZakFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZakFitTests/ZakFitTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZakFitTests.swift
+//  ZakFitTests
+//
+//  Created by Sébastien DAGUIN on 17/11/2025.
+//
+
+import Testing
+@testable import ZakFit
+
+struct ZakFitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
